a7156acbac88e57c3ba96d856cc6122f1abdb353
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-12-04 16:36:41 -0600

    created comment model and comment routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
-// const Comment = require('./Comment');
+const Comment = require('./Comment');
 
 
 // create associations
@@ -13,23 +13,23 @@ Post.belongsTo(User, {
     onDelete: 'SET NULL'
   });
 
-// Comment.belongsTo(User, {
-//     foreignKey: 'user_id',
-//     onDelete: 'SET NULL'
-//   });
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
 
-// Comment.belongsTo(Post, {
-//     foreignKey: 'post_id',
-//     onDelete: 'SET NULL'
-//   });
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
 
-// User.hasMany(Comment, {
-//     foreignKey: 'user_id',
-//     onDelete: 'SET NULL'
-//   });
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
   
-// Post.hasMany(Comment, {
-//     foreignKey: 'post_id'
-//   });
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
 
-  module.exports = { User, Post };
+  module.exports = { User, Post, Comment };
